078808055b6f647f39f4c42f6c685db9c23a3d97
Alexander Ivanovsky <sunx2ych@gmail.com>
2016-04-21 00:44:55 +0300

    Add converter logic

--- src/main/java/com/ubs/opsit/interviews/berlinclock/BerlinClockConverter.java
+++ src/main/java/com/ubs/opsit/interviews/berlinclock/BerlinClockConverter.java
@@ -1,11 +1,22 @@
 package com.ubs.opsit.interviews.berlinclock;
 
+import org.apache.commons.lang.BooleanUtils;
+
 import com.ubs.opsit.interviews.TimeConverter;
+import com.ubs.opsit.interviews.util.TimeUtils;
+
+import static com.ubs.opsit.interviews.berlinclock.BerlinClockLights.*;
 
 public class BerlinClockConverter implements TimeConverter {
 
     public String convertTime(String aTime) {
-        // TODO Implement
-        return aTime;
+        int[] timeParts = TimeUtils.parse24hClock(aTime);
+        int hours = timeParts[0], minutes = timeParts[1], seconds = timeParts[2];
+        return String.join(" ",
+                SECONDS.turnOn(BooleanUtils.toInteger(seconds % 2 == 0)),
+                HOURS_1ST_ROW.turnOn(hours / 5),
+                HOURS_2ND_ROW.turnOn(hours % 5),
+                MINUTES_1ST_ROW.turnOn(minutes / 5),
+                MINUTES_2ND_ROW.turnOn(minutes % 5));
     }
 }
